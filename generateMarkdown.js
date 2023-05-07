ceec21694f906de87d9c62e283308b988794f892
Nate Trask <trask.nate@gmail.com>
2023-05-07 11:51:46 -0500

    added comments to js files

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,4 +1,4 @@
-// DONE: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== 'none') {
@@ -19,7 +19,7 @@ function renderLicenseBadge(license) {
   return ''
 }
 
-// DONE: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== 'none') {
@@ -28,7 +28,7 @@ function renderLicenseLink(license) {
   return '';
 }
 
-// DONE: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== 'none') {
@@ -38,7 +38,7 @@ function renderLicenseSection(license) {
   return ""; 
 }
 
-// DONE: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   const licenseBadge = renderLicenseBadge(data.license);
   const licenseLink = renderLicenseLink(data.license);
